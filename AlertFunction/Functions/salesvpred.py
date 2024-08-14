5dfbd19977430f0dade7e66d9a6d30c911019cdd
Sajjan <sajjanpaudel811@gmail.com>
2024-08-14 12:50:17 +0545

    get dates dynamically

--- AlertFunction/Functions/salesvpred.py
+++ AlertFunction/Functions/salesvpred.py
@@ -2,18 +2,32 @@ import psycopg2
 import pandas as pd
 from AlertFunction.params import prod_params
 from AlertFunction.constant import restaurants
+from datetime import timedelta
 
 def sales_vs_pred():
     with psycopg2.connect(**prod_params) as conn:
         with conn.cursor() as cursor:
+            end_date_query = '''
+            SELECT MAX(gastronomic_day)
+            FROM public."SalesData"
+            WHERE restaurant = %s
+        '''
             all_data = pd.DataFrame()
             for restaurant in restaurants:
-                if restaurant in ['Restaurant','Fisketorget Utsalg']:
-                    from_date = 9
-                    to_date = 2
-                else:
-                    from_date = 8
-                    to_date = 1
+                cursor.execute(end_date_query,(restaurant,))
+                latest_gastronomic_day = cursor.fetchone()[0]
+                if latest_gastronomic_day:
+                    to_date= latest_gastronomic_day.strftime("%Y-%m-%d")
+                    from_date = (latest_gastronomic_day - timedelta(days=6)).strftime("%Y-%m-%d")
+                # if restaurant in ['Restaurant','Fisketorget Utsalg']:
+                #     from_date = 9
+                #     to_date = 2
+                # elif restaurant in ['Åsane Storsenter']:
+                #     to_date = 2
+                #     from_date = 9
+                # else:
+                #     from_date = 8
+                #     to_date = 1
                 sales_vs_pred_query = f'''
                 WITH LatestEntries AS (
                         SELECT
@@ -23,8 +37,8 @@ def sales_vs_pred():
                         FROM
                             public."Predictions_predictions"
                         WHERE
-                            date > date(CURRENT_DATE - INTERVAL '{from_date} days')
-                            and date <= date(CURRENT_DATE - INTERVAL '{to_date} days')
+                            date >= '{from_date}'
+                            and date <= '{to_date}'
                         GROUP BY
                             date, restaurant
                     ),
@@ -39,7 +53,7 @@ def sales_vs_pred():
                         ON p.date = le.date
                         AND p.restaurant = le.restaurant
                         AND p.created_at = le.latest_created_at
-                        where p.restaurant = %s
+                        where p.restaurant = '{restaurant}'
                     group BY
                         p.restaurant),
                     actual_sales as (
@@ -47,17 +61,17 @@ def sales_vs_pred():
                         FROM 
                             public."SalesData"
                         WHERE
-                            gastronomic_day > date(CURRENT_DATE - INTERVAL '{from_date} days')
-                            and gastronomic_day <= date(CURRENT_DATE - INTERVAL '{to_date} days')
-                            and restaurant = %s
+                            gastronomic_day >= '{from_date}'
+                            and gastronomic_day <= '{to_date}'
+                            and restaurant = '{restaurant}'
                         GROUP BY restaurant
                     )
 
-                    SELECT date(CURRENT_DATE - INTERVAL '{from_date} days') as to, date(CURRENT_DATE - INTERVAL '{to_date} days') as from_date, Ac.restaurant,Ac.actual_sales,Ap.actual_pred
+                    SELECT '{from_date}' as to, '{to_date}' from_date, Ac.restaurant,Ac.actual_sales,Ap.actual_pred
                         FROM actual_sales Ac  join actual_prediction Ap on Ac.restaurant = Ap.restaurant
 
                 '''
-                cursor.execute(sales_vs_pred_query,[restaurant,restaurant])
+                cursor.execute(sales_vs_pred_query)
                 rows = cursor.fetchall()
                 temp_df = pd.DataFrame(rows, columns=['from', 'to', 'restaurant', 'Sum of Sales', 'Sum of Prediction'])
                 temp_df['Percentage_change'] = ((temp_df['Sum of Sales'] - temp_df['Sum of Prediction'])/temp_df['Sum of Sales'])*100
